d94ecfb8d036f01024587682940e6977095b7288
KamaleshKannan <81850601+KamaleshKannan@users.noreply.github.com>
2021-04-11 17:14:57 +0530

    Add files via upload

--- DecisionMaking.java
+++ DecisionMaking.java
@@ -5,16 +5,18 @@ public class DecisionMaking {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int input;
-		System.out.println("Enter your response (1 to 3): ");
-		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your salary : ");
+		 * Scanner s = new Scanner(System.in);
 		input = s.nextInt();
-		switch(input) {
-		case 1  : System.out.println("hi");
-		break;
-		case 2: System.out.println("hello");
-		break;
-		case 3 : System.out.println("welcome");
-		break;
+		if(input<20000)
+			System.out.println("Your tax value is 3%");
+		else if(input<40000)
+			System.out.println("Your tax value is 5%");
+		else if(input<60000)
+			System.out.println("Your tax value is 7%");
+		else
+			System.out.println("Your tax value is 10%");
+		
 		default :System.out.println("invalid input");
 		}
 	}
